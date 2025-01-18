c0afdb1c525768bff671bd9f051487064e2f2c4f
diwakar <d.7484959595@gmail.com>
2025-01-18 13:20:55 +0530

    built a custom binary file

---
+++ app.js
@@ -0,0 +1,21 @@
+// let time = 1;
+// console.log('Hello, I am Diwakar.');
+// console.dir(process); 
+// ls -l
+setInterval(() => {
+    console.log('Hello, I am Diwakar.');
+    new Date().toLocaleString(
+        'en-US',
+        {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: true
+        }
+    )
+}, 1000);
+
+// console.log(typeof require)
+// console.log(typeof module.exports)
+console.log(firstname)
+
